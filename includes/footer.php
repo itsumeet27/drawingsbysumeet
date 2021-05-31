1c5d06d8a9dc0421c4897248145efd2079d4db16
Sumeet Sharma <itsumeet@gmail.com>
2021-05-31 23:34:07 +0530

    Space fix

--- includes/footer.php
+++ includes/footer.php
@@ -66,18 +66,18 @@
         <!--/.Copyright-->
     </footer>
 	
-	<script type="text/javascript">
-		function openNav() {
-		  document.getElementById("mySidepanel").style.width = "300px";
-		}
+  	<script type="text/javascript">
+  		function openNav() {
+  		  document.getElementById("mySidepanel").style.width = "300px";
+  		}
 
-		function closeNav() {
-		  document.getElementById("mySidepanel").style.width = "0";
-		}
-	</script>
+  		function closeNav() {
+  		  document.getElementById("mySidepanel").style.width = "0";
+  		}
+  	</script>
 
-	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-	<!-- MDB -->
+  	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  	<!-- MDB -->
     <script type="text/javascript" src="js/mdb.min.js"></script>
     <!-- Custom scripts -->
     <script type="text/javascript"></script>
